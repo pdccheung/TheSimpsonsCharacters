e44b3f33488193496edf1b131c25c23ecb14433a
pdccheung <pdccheung@gmail.com>
2021-04-01 23:07:39 -0400

    secrets out

--- simpsonscharacters/simpsonscharacters/settings.py
+++ simpsonscharacters/simpsonscharacters/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from secrets import *
+from decouple import configs
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,14 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vokg(m#(*wbwx2e06_z1vhy40deuq^qe)4)wp!k*cr+&2ysf_*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SECRET_KEY = config("SECRET_KEY")
 
 # Application definition
 
