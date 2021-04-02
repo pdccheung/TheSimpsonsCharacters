d8a8cd55005ee660fd5ee3e856216890005884ef
pdccheung <pdccheung@gmail.com>
2021-04-02 14:04:00 -0400

    second part partially done

--- simpsonscharacters/main_app/models.py
+++ simpsonscharacters/main_app/models.py
@@ -1,18 +1,14 @@
 from django.db import models
 
 # Create your models here.
-""" class Character: 
-    def __init__(self, name, age, sex, description, occupation):
-        self.name = name
-        self.age = age
-        self.sex = sex
-        self.description = description
-        self.occupation = occupation
 
-character = [
-    Character('Bart Simpson', '10', 'male', 'Bartholomew JoJo "Bart" Simpson (born April 1) is the Deuteragonist of The Simpsons.', 'student' ),
-    Character('Homer Simpson', '39', 'male', 'Homer Jay Simpson (born May 12) is the main Protagonist of The Simpsons series.', 'safety inspector'),
-    Character('Marge Simpson', '36', 'female', 'Marjorie Jacqueline "Marge" Simpson is the homemker and a full-time crazy mom of the Simpsons family', 'homemaker'),
-    Character('List Simpson', '8', 'female', 'Lisa Marie Simpson (born May 9 1981) is the elder daughter and middle child of the Simpson family', 'student'),
-    Character('Maggie Simpson', '1', 'female', 'Margaret Evelyn Lenny "Maggie" Simpson, is the 1-year-old, and youngest, child of Marge and Homer', 'baby' ),
-] """
+class Character(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.PositiveSmallIntegerField()
+    sex = models.CharField(max_length=100)
+    occupation = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+   
+
+    def __str__(self):
+        return self.name
--- simpsonscharacters/main_app/views.py
+++ simpsonscharacters/main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Character
 
 # Create your views here.
 from django.http import HttpResponse
@@ -9,22 +10,6 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
-class Character: 
-    def __init__(self, name, age, sex, description, occupation):
-        self.name = name
-        self.age = age
-        self.sex = sex
-        self.description = description
-        self.occupation = occupation
-
-characters = [
-    Character('Homer Simpson', '39', 'male', 'Homer Jay Simpson (born May 12) is the main Protagonist of The Simpsons series.', 'safety inspector'),
-    Character('Marge Simpson', '36', 'female', 'Marjorie Jacqueline "Marge" Simpson is the homemker and a full-time crazy mom of the Simpsons family', 'homemaker'),
-    Character('Bart Simpson', '10', 'male', 'Bartholomew JoJo "Bart" Simpson (born April 1) is the Deuteragonist of The Simpsons.', 'student' ),
-    Character('List Simpson', '8', 'female', 'Lisa Marie Simpson (born May 9 1981) is the elder daughter and middle child of the Simpson family', 'student'),
-    Character('Maggie Simpson', '1', 'female', 'Margaret Evelyn Lenny "Maggie" Simpson, is the 1-year-old, and youngest, child of Marge and Homer', 'baby' ),
-]
-
 def characters_index(request):
+  characters = Character.objects.all()
   return render(request, 'characters/index.html', { 'characters': characters })
-
